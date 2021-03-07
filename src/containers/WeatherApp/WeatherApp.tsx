fc9c6f2077bec641b6accee2dd965b9fbb1d08a2
Sergetestcargo <protopulos.pulatikos@yandex.ru>
2021-03-07 14:39:18 +0000

    implemented basic routing, bootstrapped pages

--- src/containers/WeatherApp/WeatherApp.tsx
+++ src/containers/WeatherApp/WeatherApp.tsx
@@ -1,4 +1,14 @@
 import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { Container } from "@material-ui/core";
+// components:
+import Header from "../Header";
+import Main from "../Main";
+import Footer from "../Footer";
+import Navigation from "../../components/Navigation";
+import NotFoundPage from "../../pages/NotFoundPage";
+import CurrentLocationPage from "../../pages/CurrentLocationPage";
+import ExtendedForecastPage from "../../pages/ExtendedForecastPage";
 // styles:
 import { useStyles } from "./style";
 
@@ -7,7 +17,26 @@ import { useStyles } from "./style";
  * @returns {JSX} markup
  */
 const WeatherApp: React.FC = () => {
-  return <div className="App">app page</div>;
+  return (
+    <BrowserRouter>
+      <Container>
+        <Header>
+          <Navigation />
+        </Header>
+        <Main>
+          <Switch>
+            <Route path="/" exact component={CurrentLocationPage} />
+            <Route
+              path="/extended-forecast/:strLocationName"
+              component={ExtendedForecastPage}
+            />
+            <Route component={NotFoundPage} />
+          </Switch>
+        </Main>
+        <Footer>footer</Footer>
+      </Container>
+    </BrowserRouter>
+  );
 };
 
 export default WeatherApp;
--- src/containers/WeatherApp/style.ts
+++ src/containers/WeatherApp/style.ts
@@ -1,7 +1,8 @@
-import { makeStyles } from "@material-ui/core/styles";
+import { makeStyles, Theme } from "@material-ui/core/styles";
 
-export const useStyles = makeStyles({
+export const useStyles = makeStyles((theme: Theme) => ({
   weatherApp: {
     display: "flex",
+    columnGap: theme.spacing(1),
   },
-});
+}));
